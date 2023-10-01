ea4a4da01ebc4fa3f2b55dd2fe24bb77369dfe2c
nicolargo <nicolas@nicolargo.com>
2023-10-01 18:39:23 +0200

    Rework Layout

--- devarena/layout_textual.py
+++ devarena/layout_textual.py
@@ -5,6 +5,7 @@ from textual import on
 from textual.app import App, ComposeResult
 from textual.binding import Binding
 from textual.events import Mount
+from textual.containers import Container
 from textual.widgets import Header, Footer, Log, DataTable, SelectionList
 from textual.widgets.selection_list import Selection
 
@@ -55,6 +56,7 @@ class KlanceLayout(App):
                           i.status.capacity['memory']))
         return nodes
 
+    @on(Mount)
     def update_nodes(self):
         nodes_plugin = self.query_one('#nodes', DataTable)
         nodes = self.k8s_nodes()
@@ -71,6 +73,7 @@ class KlanceLayout(App):
                                i.metadata.name))
         return namespaces
 
+    @on(Mount)
     def update_namespaces(self):
         self.query_one('#namespaces', SelectionList).border_title = "Select namespaces"
 
@@ -95,72 +98,95 @@ class KlanceLayout(App):
                                 i.status.pod_ip))
         return pods
 
-    @on(Mount)
     @on(SelectionList.SelectedChanged)
     def update_pods(self):
+        # Get the current selected namespaces
         selected_namespaces = self.query_one('#namespaces', SelectionList).selected
-        pods_plugin = self.query_one('#pods', DataTable)
-
         pods = self.k8s_pods(namespaces=selected_namespaces)
 
-        pods_plugin.border_title = "{} pods in namespaces {}".format(
-            len(pods) - 1 if len(pods) > 1 else 'No',
+        # Update the pods table
+        pods_plugin = self.query_one('#pods', DataTable)
+        pods_plugin.border_title = "{} in namespaces {}".format(
+            '{} pods'.format(len(pods) - 1) if len(pods) > 1 else 'No pod',
             ', '.join(selected_namespaces) if selected_namespaces != [] else '')
         pods_plugin.clear()
-        if pods != []:
-            # Update column the first time
-            # pods_plugin.columns.clear() may be better ?
-            if len(pods_plugin.columns) == 0:
-                pods_plugin.add_columns(*pods[0])
-            pods_plugin.add_rows(pods[1:])
+        # Update column the first time
+        # pods_plugin.columns.clear() may be better ?
+        if len(pods_plugin.columns) == 0:
+            pods_plugin.add_columns(*pods[0])
+        pods_plugin.add_rows(pods[1:])
 
     # LOGS
 
-    def k8s_logs(self) -> list:
+    def k8s_logs(self, namespaces=[]) -> list:
         logs = []
         for i in self.v1.list_pod_for_all_namespaces(watch=False).items:
-            logs.append(self.v1.read_namespaced_pod_log(name=i.metadata.name,
-                                                        namespace=i.metadata.namespace))
+            if i.metadata.namespace in namespaces or namespaces == []:
+                logs.append(self.v1.read_namespaced_pod_log(name=i.metadata.name,
+                                                            namespace=i.metadata.namespace))
         return logs
 
+    @on(SelectionList.SelectedChanged)
     def update_logs(self):
-        self.query_one(Log).write_lines(self.k8s_logs(),
-                                        scroll_end=None)
+        # Get the current selected namespaces
+        selected_namespaces = self.query_one('#namespaces', SelectionList).selected
+
+        logs_plugin = self.query_one('#logs', Log)
+        logs_plugin.border_title = "Logs in namespaces {}".format(
+            ', '.join(selected_namespaces) if selected_namespaces != [] else '')
+        logs_plugin.clear()
+        logs_plugin.write_lines(self.k8s_logs(namespaces=selected_namespaces),
+                                scroll_end=None)
 
     # LAYOUT (update)
 
     def on_mount(self) -> None:
-        self.update_nodes()
-        self.update_namespaces()
-        # Sure ? better to call it from callback namespace ?
-        # self.update_pods()
+        pass
 
     def on_ready(self) -> None:
+        self.update_pods()
         self.update_logs()
 
     # LAYOUT (create)
 
     def compose(self) -> ComposeResult:
+        # yield Header()
+
+        # # https://textual.textualize.io/widgets/data_table/#datatable
+        # yield DataTable(classes="box", id="nodes")
+
+        # # https://textual.textualize.io/widgets/selection_list/#selectionlist
+        # namespaces = self.k8s_namespaces()
+        # namespaces_list = [Selection(*i) for i in namespaces]
+        # yield SelectionList[str](
+        #     *namespaces_list,
+        #     classes="box",
+        #     id="namespaces"
+        # )
+
+        # # https://textual.textualize.io/widgets/data_table/#datatable
+        # yield DataTable(classes="box", id="pods")
+
+        # # https://textual.textualize.io/widgets/log/#log
+        # yield Log(classes="box", id="logs",
+        #           highlight=True, auto_scroll=True)
+
+        # yield Footer()
+
         yield Header()
-
-        # https://textual.textualize.io/widgets/data_table/#datatable
-        yield DataTable(classes="box", id="nodes")
-
-        # https://textual.textualize.io/widgets/data_table/#datatable
         namespaces = self.k8s_namespaces()
         namespaces_list = [Selection(*i) for i in namespaces]
-        yield SelectionList[str](
-            *namespaces_list,
-            classes="box",
-            id="namespaces"
-        )
-
-        # https://textual.textualize.io/widgets/data_table/#datatable
-        yield DataTable(classes="box", id="pods")
-
-        # https://textual.textualize.io/widgets/log/#log
-        yield Log(highlight=True, auto_scroll=True, id="logs")
-
+        yield Container(DataTable(classes="box", id="nodes"),
+                        Container(
+                            SelectionList[str](
+                                *namespaces_list,
+                                classes="box",
+                                id="namespaces"
+                            ),
+                            DataTable(classes="box", id="pods"),
+                            id="middle"),
+                        Log(classes="box", id="logs", highlight=True, auto_scroll=True),
+                        id="main")
         yield Footer()
 
 
